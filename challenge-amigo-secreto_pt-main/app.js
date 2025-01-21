198d59cd7fa4152aa1a85c201f3be0d171c60de7
Luana <luanaramosdsantos@gmail.com>
2025-01-20 21:29:27 -0300

    Primeiro commit - início do projeto

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,12 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+    let inputAmigo = document.getElementById('amigo').value;
+
+    if(inputAmigo == ''){
+        alert('Por favor, insira um nome!');
+    }
+
+
+}
