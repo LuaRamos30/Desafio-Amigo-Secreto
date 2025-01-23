df78b337ed536edc29e84fc187a1398c4b102e6b
Luana <luanaramosdsantos@gmail.com>
2025-01-22 21:04:24 -0300

    Adicionando função desativar botão sortear e estlização para o botão desativado

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,6 +1,19 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = []; //array vazio para armazenar os amigos
 
+
+
+//função para desativar o botão sortear quando a lista tiver vazia
+function atualizarBotaoSortear() {
+    let botaoSortear = document.querySelector(".button-draw"); //pegar o botão sortear
+    if (listaAmigos.length === 0){
+        botaoSortear.disabled = true; //desabilitar o botão se a lista de amigos estiver vazia
+    }else{
+        botaoSortear.disabled = false; //habilitar o botão se a lista de amigos não estiver vazia
+    }
+
+}
+
+///função para adicionar amigos
 function adicionarAmigo() {
   let inputAmigo = document.getElementById("amigo").value.trim(); //input para adicionar amigos
 
@@ -16,34 +29,44 @@ function adicionarAmigo() {
 
   listaAmigos.push(inputAmigo); //adiciona o nome do amigo no array
 
-    atualizarAmigos(); //atualizar a lista de amigos
+  atualizarAmigos(); //atualiza a lista de amigos
+    atualizarBotaoSortear(); //atualiza o botão sortear
 
-  //limpar o campo
-  document.getElementById("amigo").value = "";
+  document.getElementById("amigo").value = "";  //limpa o campo
 }
+
+
+
+
+
+
 
 function atualizarAmigos() {
   document.getElementById("listaAmigos").innerHTML = ""; //limpar a lista de amigos
- 
-  listaAmigos.forEach((amigo) => {
+
+  listaAmigos.forEach((amigo) => { //para cada amigo na lista de amigos
     let li = document.createElement("li"); //criar um elemento li
     li.innerHTML = amigo; //adicionar o nome do amigo no li
     document.getElementById("listaAmigos").appendChild(li); //adicionar o li na lista de amigos
   });
-  
-
-
 }
 
+
+
 function sortearAmigo() {
-  if (listaAmigos.length === 0) {
+  if (listaAmigos.length === 0) { //verificar se a lista de amigos está vazia
     alert("Adicione amigos antes de sortear!");
     return;
   }
 
-  let amigoSorteado =
+  let amigoSorteado = 
     listaAmigos[Math.floor(Math.random() * listaAmigos.length)]; //sortear um amigo
-  document.getElementById(
+  document.getElementById( //pegar o elemento com id
     "resultado"
   ).innerHTML = `Amigo Sorteado: ${amigoSorteado}`; //mostrar o amigo sorteado na tela
 }
+
+
+
+// Adiciona um evento que executa a função 'atualizarBotaoSortear' assim que o documento HTML estiver completamente carregado.
+document.addEventListener("DOMContentLoaded", atualizarBotaoSortear);
